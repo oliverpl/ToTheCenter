8ef4a5317e593e66b71c55cf419091e42dafbdf0
oliverpl <patrick.l.oliver@gmail.com>
2017-04-19 19:41:21 -0700

    Initial game commit
    Includes title screen and the ability to move to the main game; but no
    other functionality included.

---
+++ Assets/MainMenu/ButtonManager.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ButtonManager : MonoBehaviour
+{
+	public void NewGameButton(string newGameLevel)
+    {
+        SceneManager.LoadScene(newGameLevel);
+    }
+
+    public void ExitGameButton()
+    {
+        Application.Quit();
+    }
+}
