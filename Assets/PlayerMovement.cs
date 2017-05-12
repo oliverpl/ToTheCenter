3c90c476f02738166f4b1d4d70c29baa76296c46
oliverpl <patrick.l.oliver@gmail.com>
2017-05-12 00:03:46 -0700

    Added player movment.
    Player movement has been started.

---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+    private Rigidbody2D playerCollisionBox;
+    public float speed = 0.05F;
+
+    public void Awake()
+    {
+        playerCollisionBox = GetComponent<Rigidbody2D>();
+    }
+
+    public void Update()
+    {
+        var mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        transform.position = Vector3.MoveTowards(transform.position, mousePosition, speed);
+    }
+}
