b8e2defb031ca8aea2ca9c60ad13ff370a269ade
oliverpl <patrick.l.oliver@gmail.com>
2017-06-19 23:14:59 -0700

    Added collision detection
    Today I wrote enough code to get collision detection working. Problem is
    that it seems to only work in a line from a single point and not from
    the player object.
    
    Tomorrow's goal is to fix collision detection.

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -3,17 +3,55 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour {
-    private Rigidbody2D playerCollisionBox;
+    //private CircleCollider2D playerCollisionBox;
+    private object arcCollisionBox;
     public float speed = 0.05F;
+    public Dictionary<string, KeyCode> movementMap;
 
     public void Awake()
     {
-        playerCollisionBox = GetComponent<Rigidbody2D>();
+        //playerCollisionBox = GetComponent<CircleCollider2D>();
+        arcCollisionBox = GetComponent<arcBehavior>();
+        movementMap = new Dictionary<string, KeyCode>
+        {
+            {"up", KeyCode.W },
+            {"down", KeyCode.D },
+            {"left", KeyCode.L },
+            {"right", KeyCode.R }
+        };
     }
 
     public void Update()
     {
-        var mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-        transform.position = Vector3.MoveTowards(transform.position, mousePosition, speed);
+        var playerCollisionBox = GetComponent<CircleCollider2D>();
+        var mousePosition = (Vector2)Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        var direction = (mousePosition - playerCollisionBox.offset.normalized).normalized;
+        //var hit = Physics2D.Linecast(playerCollisionBox.offset.normalized, mousePosition, LayerMask.GetMask("Arc"));
+        var hit = Physics2D.CircleCast(playerCollisionBox.offset, playerCollisionBox.radius-0.5f, direction, Vector2.Distance(mousePosition, playerCollisionBox.offset),LayerMask.GetMask("Arc"));
+        if (hit.collider != null)
+        {
+            Debug.Log("Hit point: " + hit.point);
+            //playerCollisionBox.transform.Translate(hit.point);
+            playerCollisionBox.transform.position = new Vector2(hit.point.x+playerCollisionBox.radius-0.35f, hit.point.y);
+        }
+        else
+        {
+            Debug.Log("Miss point: " + mousePosition);
+            //playerCollisionBox.transform.Translate(mousePosition);
+            playerCollisionBox.transform.position = mousePosition;
+        }
+    }
+
+    private string MovementDirection()
+    {
+        foreach (var key in movementMap.Keys)
+        {
+            if (Input.GetKeyDown(movementMap[key]))
+            {
+                return key;
+            }
+        }
+
+        return string.Empty;
     }
 }
---
+++ Assets/arcBehavior.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class arcBehavior : MonoBehaviour
+{
+    public PolygonCollider2D ArcCollision { get; set; }
+
+    private void Awake()
+    {
+        ArcCollision = GetComponent<PolygonCollider2D>();
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
