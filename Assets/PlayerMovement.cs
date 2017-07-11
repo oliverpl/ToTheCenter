d50b42299fc14de3908d27c4502b51a4bd413690
oliverpl <patrick.l.oliver@gmail.com>
2017-07-10 22:52:34 -0700

    Setup win/loss condition
    Created a way to determine if the player won or lost by finding the
    center of an arc.
    
    Next up, creating scenes to indicate a win or loss.

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -4,27 +4,33 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour {
     private CircleCollider2D playerCollisionBox;
-    private object arcCollisionBox;
     private float maxCollisionDrawOffset;
+    private PolygonCollider2D arc;
+    private Vector2 arcCenter;
     public float speed = 0.05F;
-    public Dictionary<string, KeyCode> movementMap;
 
     public void Awake()
     {
         playerCollisionBox = GetComponent<CircleCollider2D>();
         maxCollisionDrawOffset = playerCollisionBox.radius - 0.35f;
-        arcCollisionBox = FindObjectOfType<arcBehavior>();
+        arcCenter = GameObject.FindWithTag("FirstArc").GetComponent<PolygonCollider2D>().transform.position;
     }
 
     public void Update()
     {
         var playerPosition = transform.position;
+        if (CheckWinCondition(playerPosition))
+        {
+            Debug.Log("Winner!");
+        }
+
         var mousePosition = (Vector2)Camera.main.ScreenToWorldPoint(Input.mousePosition);
         var direction = (mousePosition - (Vector2)playerPosition).normalized;
         var hit = Physics2D.Raycast(playerPosition, direction, Vector2.Distance(mousePosition, playerPosition) + (playerCollisionBox.radius - 0.45f), LayerMask.GetMask("Arc"));
         if (hit.collider != null)
         {
-            Debug.Log("Hit point: " + hit.point);
+            Debug.Log("Loser!");
+            //Debug.Log("Hit point: " + hit.point);
             var drawOffset = new Vector2(direction.x * maxCollisionDrawOffset * -1, direction.y * maxCollisionDrawOffset * -1);
             //Debug.Log("Draw Offset: " + drawOffset);
             transform.position = hit.point + drawOffset;
@@ -34,5 +40,11 @@ public class PlayerMovement : MonoBehaviour {
             //Debug.Log("Miss point: " + mousePosition);
             playerCollisionBox.transform.position = mousePosition;
         }
+    }
+
+    public bool CheckWinCondition(Vector2 playerPosition)
+    {
+        var distanceToCenter = Vector2.Distance(playerPosition, arcCenter);
+        return distanceToCenter < 2;
     }
 }
